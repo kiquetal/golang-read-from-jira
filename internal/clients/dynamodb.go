4fdf751c0f04333bf7d1dbd20025dce1a06ab490
kiquetal <kiquetal@gmail.com>
2025-04-13 11:28:58 -0300

    Adding creation of localstack client

--- internal/clients/dynamodb.go
+++ internal/clients/dynamodb.go
@@ -3,8 +3,10 @@ package clients
 import (
 	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"os"
 )
 
 type DynamoDBClient struct {
@@ -17,11 +19,28 @@ type DynamoDBClient struct {
 func NewDynamoDBClient() (*DynamoDBClient, error) {
 	// Initialize the DynamoDB client
 	// This is a placeholder and should be replaced with actual initialization code
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+
+	isLocal := os.Getenv("IS_LOCAL")
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"))
+
 	if err != nil {
 		return nil, fmt.Errorf("Unable to load AWS SDK config, "+"please check your AWS credentials and region: %v", err)
 
 	}
+
+	if isLocal == "true" {
+
+		client := dynamodb.NewFromConfig(cfg,
+			func(options *dynamodb.Options) {
+				options.BaseEndpoint = aws.String("http://localhost:4566")
+			})
+
+		return &DynamoDBClient{
+			client: client,
+		}, nil
+	}
+
 	return &DynamoDBClient{
 		client: dynamodb.NewFromConfig(cfg),
 	}, nil
